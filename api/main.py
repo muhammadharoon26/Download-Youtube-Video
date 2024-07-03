6986e2aca5ab69df81864cf9adc236263cf8bba9
Muhammad Haroon <mharoon1326@gmail.com>
2024-07-03 12:57:26 +0500

    thumbnail url

--- api/main.py
+++ api/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, Query
 import yt_dlp
 
-def get_download_links(video_url: str):
-    video_links = {}
-    audio_links = {}
+app = FastAPI()
 
+def get_download_links(video_url: str):
     ydl_opts = {
         'quiet': True,
         'format': 'bestvideo+bestaudio/best',
@@ -12,14 +11,16 @@ def get_download_links(video_url: str):
         'get_url': True,
     }
 
+    video_links = {}
+    audio_links = {}
+    thumbnail_url = ""
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         result = ydl.extract_info(video_url, download=False)
 
         if 'entries' in result:
-            # This indicates that the URL is a playlist
-            index = 0
-            for entry in result['entries']:
-                index += 1
+            # URL is a playlist
+            for index, entry in enumerate(result['entries'], start=1):
                 v_links, a_links = process_video(entry, index)
                 video_links.update(v_links)
                 audio_links.update(a_links)
@@ -29,52 +30,37 @@ def get_download_links(video_url: str):
             video_links.update(v_links)
             audio_links.update(a_links)
 
-    return video_links, audio_links
+        thumbnail_url = result.get('thumbnail', "")
+
+    return video_links, audio_links, thumbnail_url
 
 def process_video(video, index=None):
     formats = video.get('formats', [])
-    resolutions = set()
     video_links = {}
     audio_links = {}
 
-    # Process 360p separately
+    # Handle 360p separately
     for f in formats:
         if f.get('format_id') == '18':
-            video_links['360'] = f['url']
-            resolutions.add('360')
+            key_prefix = '360' if index is None else f'360_{index}'
+            video_links[key_prefix] = f['url']
             break
 
-    # Process other resolutions and find audio links
     for f in formats:
-        if f.get('height') and f.get('vcodec') != 'none' and f.get('protocol') == 'https':
-            resolution = str(f['height'])
-            if resolution not in video_links and resolution != '360':
-                video_links[resolution] = f['url']
-                resolutions.add(resolution)
+        if f.get('vcodec') != 'none' and f.get('acodec') == 'none' and f.get('protocol') == 'https':
+            resolution = f.get('height', 'Unknown')
+            key_prefix = f"{resolution}" if index is None else f"{resolution}_{index}"
+            if key_prefix not in video_links and resolution != 360:
+                video_links[key_prefix] = f['url']
         elif f.get('acodec') != 'none' and f.get('vcodec') == 'none' and f.get('protocol') == 'https':
-            bitrate = f.get('tbr', 0)  # Get the average bitrate
-            rounded_bitrate = round(bitrate)
-            if rounded_bitrate > 0:
-                audio_links[rounded_bitrate] = f['url']
+            bitrate = round(f.get('tbr', 0))
+            audio_key = f"Audio_{bitrate}kbps" if index is None else f"Audio_{bitrate}kbps_{index}"
+            if bitrate > 0:
+                audio_links[audio_key] = f['url']
 
-    result_video_links = {}
-    result_audio_links = {}
-
-    for res in sorted(resolutions, key=int):
-        key_prefix = f"{res}" if index is None else f"{res}_{index}"
-        result_video_links[key_prefix] = video_links[res]
-
-    for bitrate, link in audio_links.items():
-        audio_key = f"Audio_{bitrate}kbps" if index is None else f"Audio_{bitrate}kbps_{index}"
-        result_audio_links[audio_key] = link
-
-    return result_video_links, result_audio_links
-
-app = FastAPI()
+    return video_links, audio_links
 
 @app.get("/download_links")
-async def download_links(
-    video_url: str = Query(..., description="YouTube video or playlist URL"),
-):
-    video_links, audio_links = get_download_links(video_url)
-    return {"Video Links": video_links, "Audio Links": audio_links}
+async def download_links(video_url: str = Query(..., description="YouTube video or playlist URL")):
+    video_links, audio_links, thumbnail_url = get_download_links(video_url)
+    return {"Video Links": video_links, "Audio Links": audio_links, "Thumbnail": thumbnail_url}
