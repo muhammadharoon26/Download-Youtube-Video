d83005b579ca745eed3a89c29c42ebbec15b6dc6
Muhammad Haroon <mharoon1326@gmail.com>
2024-08-08 03:41:49 +0500

    re

--- api/main.py
+++ api/main.py
@@ -11,8 +11,8 @@ def get_download_links(video_url: str):
         'simulate': True,
         'get_url': True,
         'verbose': True,
-        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'nocheckcertificate': True
+        # 'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        # 'nocheckcertificate': True
     }
 
     video_links = {}
